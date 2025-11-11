cbd69f0456462e124f97a093827593d02ba676d0
smiley <jsl83@cornell.edu>
2025-11-11 16:55:57 -0500

    home screen

--- screens/home_screen.py
+++ screens/home_screen.py
@@ -1,6 +1,7 @@
 import arcade
 import arcade.gui
 from util import *
+from screens.action_button import ActionButton
 
 IMAGE_PATH_ROOT = ":resources:eldritch/images/"
 
@@ -14,16 +15,18 @@ class HomeScreen(arcade.View):
         self.layout = arcade.gui.UILayout(width=1280, height=800)
         self.manager.add(self.layout)
         cover = arcade.gui.UITextureButton(texture=arcade.load_texture(IMAGE_PATH_ROOT + 'home.png'))
-        self.host_button = arcade.gui.UITextureButton(text='Host Game', width=200, height=50, x=944, y=600, texture=arcade.load_texture(IMAGE_PATH_ROOT + '/buttons/placeholder.png'))
-        self.join_button = arcade.gui.UITextureButton(text='Join Game', width=200, height=50, x=944, y=450, texture=arcade.load_texture(IMAGE_PATH_ROOT + '/buttons/placeholder.png'))
-        self.ip_button = arcade.gui.UIInputText(width=200, height=50, x=944, y=390).with_background(arcade.load_texture(IMAGE_PATH_ROOT + '/buttons/pressed_placeholder.png'))
+        background = arcade.gui.UITextureButton(texture=arcade.load_texture(IMAGE_PATH_ROOT + 'gui/home.png'), x=808)
+        blank = arcade.load_texture(IMAGE_PATH_ROOT + 'blank.png')
+        self.host_button = ActionButton(text='Host Game', width=300, height=64, x=894, y=500, texture=blank, font='Typical Writer', style={'font_color': arcade.color.BLACK, 'font_size': 20}, bold=True)
+        self.join_button = ActionButton(text='Join Game', width=300, height=64, x=894, y=350, texture=blank, font='Typical Writer', style={'font_color': arcade.color.BLACK, 'font_size': 20}, bold=True)
+        self.ip_button = ActionButton(width=300, height=50, x=894, y=310, texture=blank, font='Poster Bodoni', style={'font_color': arcade.color.GRAY, 'font_size': 12}, text='Type host IP')
         self.click_time = 0
-        for buttons in [cover, self.host_button, self.join_button, self.ip_button]:
+        for buttons in [cover, background, self.host_button, self.join_button, self.ip_button]:
             self.layout.add(buttons)
         self.host_action = host_action
         self.join_action = join_action
 
-        self.host_action(self.window)
+        #self.host_action(self.window)
     
     def on_draw(self):
         self.clear()        
@@ -42,3 +45,26 @@ class HomeScreen(arcade.View):
                     self.host_action(self.window)
                 elif buttons[0] == self.join_button and self.ip_button.children[0].text != '':
                     self.join_action(self.ip_button.children[0].text, self.window)
+
+    def on_key_press(self, symbol, modifiers):
+        added_char = None
+        if symbol == 65288 and len(self.ip_button.text) > 0:
+            new = self.ip_button.text[:-1]
+        elif symbol in list(range(65456, 65466)) + [65454]:
+            symbol -= 65408
+            added_char = chr(symbol)
+        elif symbol in list(range(48, 58)) + [46]:
+            added_char = chr(symbol)
+        else:
+            return
+        if added_char:
+            if self.ip_button.text == 'Type host IP':
+                self.ip_button.text = ''
+                self.ip_button.set_style(arcade.color.BLACK, 20)
+            new = self.ip_button.text + added_char
+        self.ip_button.text = ''
+        self.manager.trigger_render()
+        self.ip_button.text = new
+        if self.ip_button.text == '':
+            self.ip_button.text = 'Type host IP'
+            self.ip_button.set_style(arcade.color.GRAY, 15)
